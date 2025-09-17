66df81f15bfc542be25212a00af93e5484901d5f
Richard Higgins <richard@relh.net>
2025-09-16 21:59:00 -0700

    deleted everything ui

--- scripts/build_web.sh
+++ scripts/build_web.sh
@@ -7,7 +7,7 @@ mkdir -p "$OUT_DIR"
 TARGET_HTML="$OUT_DIR/tribal_village.html"
 
 nim c \
-  --app:gui \
+  --app:console \
   --cpu:wasm32 \
   --os:standalone \
   --threads:off \
@@ -16,11 +16,8 @@ nim c \
   -d:nimNoGetRandom \
   -d:nimNoSysrand \
   -d:emscripten \
-  --passL:"-sUSE_GLFW=3" \
-  --passL:"-sUSE_WEBGL2=1" \
   --passL:"-sASYNCIFY" \
   --passL:"-sALLOW_MEMORY_GROWTH" \
-  --passL:"-sINITIAL_MEMORY=512MB" \
   --passL:"--shell-file=$ROOT_DIR/../scripts/minshell.html" \
   -o:"$TARGET_HTML" \
   "$ROOT_DIR/tribal_village.nim"
