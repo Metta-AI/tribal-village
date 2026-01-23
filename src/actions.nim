e04848a11def63ee2ad0079245048b411f92bc08
Richard Higgins <richard@softmax.com>
2026-01-23 12:11:55 -0800

    Name unit attack tint entries

--- src/actions.nim
+++ src/actions.nim
@@ -1,27 +1,42 @@
 # This file is included by src/step.nim
-const UnitAttackTints: array[AgentUnitClass, tuple[tint: TileColor, duration: int8, code: uint8]] = [
+type UnitAttackTint = object
+  tint: TileColor
+  duration: int8
+  code: uint8
+
+const UnitAttackTints: array[AgentUnitClass, UnitAttackTint] = [
   # UnitVillager
-  (TileColor(r: 0.95, g: 0.25, b: 0.20, intensity: 1.10), 1'i8, ActionTintAttackVillager),
+  UnitAttackTint(tint: TileColor(r: 0.95, g: 0.25, b: 0.20, intensity: 1.10),
+    duration: 1'i8, code: ActionTintAttackVillager),
   # UnitManAtArms
-  (TileColor(r: 0.95, g: 0.55, b: 0.15, intensity: 1.10), 2'i8, ActionTintAttackManAtArms),
+  UnitAttackTint(tint: TileColor(r: 0.95, g: 0.55, b: 0.15, intensity: 1.10),
+    duration: 2'i8, code: ActionTintAttackManAtArms),
   # UnitArcher
-  (TileColor(r: 0.95, g: 0.85, b: 0.20, intensity: 1.10), 2'i8, ActionTintAttackArcher),
+  UnitAttackTint(tint: TileColor(r: 0.95, g: 0.85, b: 0.20, intensity: 1.10),
+    duration: 2'i8, code: ActionTintAttackArcher),
   # UnitScout
-  (TileColor(r: 0.30, g: 0.90, b: 0.30, intensity: 1.10), 1'i8, ActionTintAttackScout),
+  UnitAttackTint(tint: TileColor(r: 0.30, g: 0.90, b: 0.30, intensity: 1.10),
+    duration: 1'i8, code: ActionTintAttackScout),
   # UnitKnight
-  (TileColor(r: 0.20, g: 0.90, b: 0.85, intensity: 1.12), 3'i8, ActionTintAttackKnight),
+  UnitAttackTint(tint: TileColor(r: 0.20, g: 0.90, b: 0.85, intensity: 1.12),
+    duration: 3'i8, code: ActionTintAttackKnight),
   # UnitMonk
-  (TileColor(r: 0.25, g: 0.55, b: 0.95, intensity: 1.12), 2'i8, ActionTintAttackMonk),
+  UnitAttackTint(tint: TileColor(r: 0.25, g: 0.55, b: 0.95, intensity: 1.12),
+    duration: 2'i8, code: ActionTintAttackMonk),
   # UnitBatteringRam
-  (TileColor(r: 0.45, g: 0.35, b: 0.90, intensity: 1.12), 3'i8, ActionTintAttackBatteringRam),
+  UnitAttackTint(tint: TileColor(r: 0.45, g: 0.35, b: 0.90, intensity: 1.12),
+    duration: 3'i8, code: ActionTintAttackBatteringRam),
   # UnitMangonel
-  (TileColor(r: 0.75, g: 0.35, b: 0.95, intensity: 1.18), 3'i8, ActionTintAttackMangonel),
+  UnitAttackTint(tint: TileColor(r: 0.75, g: 0.35, b: 0.95, intensity: 1.18),
+    duration: 3'i8, code: ActionTintAttackMangonel),
   # UnitGoblin
-  (TileColor(r: 0.35, g: 0.85, b: 0.35, intensity: 1.12), 1'i8, ActionTintAttackVillager),
+  UnitAttackTint(tint: TileColor(r: 0.35, g: 0.85, b: 0.35, intensity: 1.12),
+    duration: 1'i8, code: ActionTintAttackVillager),
   # UnitBoat
-  (TileColor(r: 0.95, g: 0.35, b: 0.75, intensity: 1.12), 2'i8, ActionTintAttackBoat),
+  UnitAttackTint(tint: TileColor(r: 0.95, g: 0.35, b: 0.75, intensity: 1.12),
+    duration: 2'i8, code: ActionTintAttackBoat),
 ]
 
 proc applyUnitAttackTint(env: Environment, unit: AgentUnitClass, pos: IVec2) {.inline.} =
-  let (tint, duration, code) = UnitAttackTints[unit]
-  env.applyActionTint(pos, tint, duration, code)
+  let entry = UnitAttackTints[unit]
+  env.applyActionTint(pos, entry.tint, entry.duration, entry.code)
