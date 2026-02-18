ad552924f6336c2155aa827d26f39033dfeffa03
Richard Higgins <richard@relh.net>
2026-02-17 19:34:50 -0500

    feat: Add time-bound memoization for evaluation caching (#264)
    From polecat angharad - tv-ugx3ov

--- tribal_village_env/__init__.py
+++ tribal_village_env/__init__.py
@@ -2,5 +2,19 @@
 
 from tribal_village_env.build import ensure_nim_library_current
 from tribal_village_env.environment import TribalVillageEnv, make_tribal_village_env
+from tribal_village_env.memoization import (
+    TimedMemoCache,
+    create_frame_cache,
+    memoize,
+    memoize_sync,
+)
 
-__all__ = ["TribalVillageEnv", "make_tribal_village_env", "ensure_nim_library_current"]
+__all__ = [
+    "TribalVillageEnv",
+    "make_tribal_village_env",
+    "ensure_nim_library_current",
+    "TimedMemoCache",
+    "create_frame_cache",
+    "memoize",
+    "memoize_sync",
+]
